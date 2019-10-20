a235e1b6241359c7ed7720c019d2a140cdd08f79
0xd34b33f <0xd34b33f@users.noreply.github.com>
2019-10-20 19:40:55 +0300

    Damn love this lang. Won't compile no matter you doing :)

--- server/src/config_reader/mod.rs
+++ server/src/config_reader/mod.rs
@@ -1,20 +1,43 @@
 use std::path::Path;
 use signal::Signal;
+use serde_yaml::from_reader;
+use std::fs::File;
+use std::io::prelude::*;
 
-pub struct Task<'t> {
-	program_name: &'t String,
-	program_path: &'t Path,
+pub struct Task<'i> {
+	program_name: String,
+	program_path: &'i Path,
 	numprocs: u16,
-	woking_dir: &'t Path,
+	woking_dir: &'i Path,
 	autostart: bool,
 	autorestart: bool,
-	exitcodes: &'t Vec<u8>,
+	exitcodes: Vec<u8>,
 	startretries: u16,
 	starttime: u16,
-	stopsignal: &'t Signal,
+	stopsignal: Signal,
 }
 
-pub type task_list<'a> = Vec<Task>;
+pub struct TaskList<'i>(Vec<Task<'i>>);
 
-pub fn read_config()
-{}
+impl<'i> TaskList<'i> {
+	pub fn read_config(&mut self, config_path: &Path)
+	{
+		let mut file = File::open(config_path);
+		let file = match file {
+			Ok(f) => f,
+			Err(er) => {
+				eprintln!("Error opening {}", config_path.display());
+				std::process::exit(1);
+			}
+		};
+		let parse_res = from_reader(file);
+		let parse_res:String = match parse_res {
+			Ok(r) => parse_res,
+			Err(e) => {
+				eprintln!("Error parsing {}", config_path.display());
+				std::process::exit(1);
+			}
+		};
+	}
+}
+
