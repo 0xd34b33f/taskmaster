e3866d28f12866a403abff2e0e712aa6ddc2b1c6
0xd34b33f <0xd34b33f@users.noreply.github.com>
2019-11-10 22:41:33 +0300

    Build failing, finding out how to create stdio from Pathbuf

--- server/src/task_mangment/mod.rs
+++ server/src/task_mangment/mod.rs
@@ -1,19 +1,30 @@
-use std::path::PathBuf;
 use crate::config_reader::{read_config, Task};
-
+use log::{debug, error, info, trace, warn};
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
 
 struct ProcessInfo {
-	pid: u32,
+    pid: u32,
 }
 
 struct TaskState {
-	task_info: Task,
-	procceses: Vec<ProcessInfo>,
+    task_info: Task,
+    procceses: Vec<ProcessInfo>,
 }
 
-
+fn spawn_process(task: Task) {
+    debug!("Started spawning {}", task.program_name);
+    let cmd = task.program_path.split_whitespace();
+    let mut programm_prototype = Command::new(task.program_name).args(cmd).envs(task.env);
+    if task.stdout.is_some() {
+        programm_prototype.stdout(task.stdout.unwrap());
+    }
+    Command::new(task.program_name);
+}
 
 pub fn mange_tasks(config_path: PathBuf) {
-	let tasks = read_config(config_path.as_path());
-	
+    let tasks = read_config(Stdio::);
+    for task in tasks {
+        spawn_process(task);
+    }
 }
